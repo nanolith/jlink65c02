889ff5d2534f7ace08b3d73a092a44361a289dd7
Justin Handville <nanolith@gmail.com>
2023-09-04 19:52:57 -0400

    impl of bootstrap_linker constructor.

---
+++ src/bootstrap_linker/bootstrap_linker.cpp
@@ -0,0 +1,22 @@
+/**
+ * \file src/bootstrap_linker/bootstrap_link.cpp
+ *
+ * \brief Constructor for the bootstrap linker.
+ *
+ * \copyright 2023 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <jlink65c02/bootstrap_linker.h>
+
+using namespace jlink65c02;
+using namespace std;
+
+bootstrap_linker::bootstrap_linker()
+    : text_address(0x0000)
+    , curr_text_address(0x0000)
+    , data_address(0x0000)
+    , text_address_set(false)
+    , in_absolute_address(false)
+{
+}
