926080ee607e63f7fe1a575bf51b88680011fe42
Justin Handville <nanolith@gmail.com>
2023-09-06 23:00:56 -0400

    impl of bootstrap_linker::decodeLCommand2.

---
+++ src/bootstrap_linker/decodeLCommand2.cpp
@@ -0,0 +1,20 @@
+/**
+ * \file src/bootstrap_linker/decodeLCommand2.cpp
+ *
+ * \brief Decode an L command in the second linker pass.
+ *
+ * \copyright 2023 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <jlink65c02/bootstrap_linker.h>
+
+using namespace jlink65c02;
+using namespace std;
+
+void bootstrap_linker::decodeLCommand2(istream& in, ostream& log, ostream& out)
+{
+    (void)in;
+    (void)log;
+    (void)out;
+}
